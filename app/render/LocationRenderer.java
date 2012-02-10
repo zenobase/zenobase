0a868809cecad5ac038980ccbed89684d9d6440c
Eric Jain <ejain@zenobase.com>
2012-02-10 10:28:41 -0800

    changed default zoom level and map type for Google Maps link

--- app/render/LocationRenderer.java
+++ app/render/LocationRenderer.java
@@ -13,7 +13,7 @@ public class LocationRenderer extends Renderer<Location> {
 
 	@Override
 	public void render(Field<Location> field, Location value, PrintWriter out) {
-		out.printf("<i class=\"icon-map-marker\" title=\"%s\"></i> <a href=\"http://maps.google.com/maps?q=%s,%s&z=13\">%s, %s</a>",
+		out.printf("<i class=\"icon-map-marker\" title=\"%s\"></i> <a href=\"http://maps.google.com/maps?q=%s,%s&t=p&z=5\">%s, %s</a>",
 			field.getName(), value.getLatitude(), value.getLongitude(), value.getLatitude(), value.getLongitude());
 	}
 }
