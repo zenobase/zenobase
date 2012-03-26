2447e9d05d85802e5f3d34476496dad6de949c8d
Eric Jain <ejain@zenobase.com>
2012-03-26 14:31:11 -0700

    added admin page

---
+++ public/javascripts/zeno-admin.js
@@ -0,0 +1,34 @@
+HistoryCtrl.$inject = ['$scope', '$http'];
+function HistoryCtrl($scope, $http) {
+
+	$scope.offset = 0;
+	$scope.limit = 10;
+	$scope.history = [];
+
+	$scope.hasPrev = function() {
+		return $scope.offset > 0;
+	}
+	$scope.hasNext = function() {
+		return $scope.history && $scope.offset + $scope.limit < $scope.history.total;
+	}
+	$scope.prev = function() {
+		$scope.refresh({ offset : $scope.offset - $scope.limit });
+	}
+	$scope.next = function() {
+		$scope.refresh({ offset : $scope.offset + $scope.limit });
+	}
+	$scope.params = function() {
+		return {
+			offset : $scope.offset,
+			limit : $scope.limit
+		};
+	}
+	$scope.refresh = function(params) {
+		$http.get('/queue/?' + $.param($.extend($scope.params(), params))).success(function(response) {
+			$.extend($scope, params);
+			$scope.history = response;
+		});
+	};
+
+	$scope.refresh({});
+}
