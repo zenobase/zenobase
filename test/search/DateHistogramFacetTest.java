086c68dbef1f3b3502d985db288b97ba61ff69a6
Eric Jain <ejain@zenobase.com>
2012-03-31 10:42:02 -0700

    refactored unit test

--- test/search/DateHistogramFacetTest.java
+++ test/search/DateHistogramFacetTest.java
@@ -1,8 +1,12 @@
 package search;
 
+import java.util.Map;
+
 import junit.framework.Assert;
 
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
@@ -11,49 +15,73 @@ import org.elasticsearch.search.facet.FacetBuilders;
 import org.elasticsearch.search.facet.datehistogram.DateHistogramFacet;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 public class DateHistogramFacetTest {
+
+	private Node node;
+	private Client client;
+	private final DateTimeZone timezone = DateTimeZone.forOffsetHours(-8);
+	private final String cluster = "test";
+	private final String index = "test";
+	private final String type = "type";
+	private final String field = "date";
+
+	@Before
+	public void init() {
+		node = NodeBuilder.nodeBuilder().clusterName(cluster).local(true)
+			.settings(ImmutableSettings.settingsBuilder().put("gateway.type", "none")).node();
+		client = node.client();
+	}
 
 	@Test
 	public void test() {
 
-		DateTimeZone timezone = DateTimeZone.forOffsetHours(-8);
+		String value = "2012-03-31T20:15:30-08:00";
 
-		Node node = NodeBuilder.nodeBuilder().clusterName("test").local(true)
-			.settings(ImmutableSettings.settingsBuilder().put("gateway.type", "none")).node();
-		node.client().prepareIndex("test", "type", "1")
-			.setSource("date", "2012-03-31T20:15:30-08:00").execute().actionGet();
-		node.client().admin().indices().prepareRefresh().execute().actionGet();
+		index(value);
 
-		SearchResponse response = node.client().prepareSearch("test")
-			.setQuery(QueryBuilders.matchAllQuery())
-			.addFacet(FacetBuilders.dateHistogramFacet("years").field("date").interval("year").preZone(timezone.toString()))
-			.addFacet(FacetBuilders.dateHistogramFacet("months").field("date").interval("month").preZone(timezone.toString()))
-			.addFacet(FacetBuilders.dateHistogramFacet("days").field("date").interval("day").preZone(timezone.toString()))
-			.addFacet(FacetBuilders.dateHistogramFacet("hours").field("date").interval("hour").preZone(timezone.toString()))
-			.addFacet(FacetBuilders.dateHistogramFacet("minutes").field("date").interval("minute").preZone(timezone.toString()))
-			.execute().actionGet();
+		ImmutableMap<String, DateTime> intervals = ImmutableMap.<String, DateTime>builder()
+			.put("year", new DateTime(2012, 1, 1, 0, 0, timezone))
+			.put("month", new DateTime(2012, 3, 1, 0, 0, timezone))
+			.put("day", new DateTime(2012, 3, 31, 0, 0, timezone))
+			.put("hour", new DateTime(2012, 3, 31, 20, 0, timezone))
+			.put("minute", new DateTime(2012, 3, 31, 20, 15, timezone))
+			.build();
 
-		assertFacetDateTimeEquals(new DateTime(2012, 1, 1, 0, 0, timezone), response, "years", true);
-		assertFacetDateTimeEquals(new DateTime(2012, 3, 1, 0, 0, timezone), response, "months", true);
-		assertFacetDateTimeEquals(new DateTime(2012, 3, 31, 0, 0, timezone), response, "days", true);
-		assertFacetDateTimeEquals(new DateTime(2012, 3, 31, 20, 0, timezone), response, "hours", false);
-		assertFacetDateTimeEquals(new DateTime(2012, 3, 31, 20, 15, timezone), response, "minutes", false);
-
-		node.close();
-	}
-
-	private void assertFacetDateTimeEquals(DateTime expected, SearchResponse response, String facetName, boolean correctOffset) {
-		DateHistogramFacet facet = response.facets().facet(facetName);
-		long time = facet.getEntries().get(0).time();
-		if (correctOffset) {
-			time = time - expected.getZone().getOffset(time);
+		SearchResponse response = search(intervals);
+		for (Map.Entry<String, DateTime> entry : intervals.entrySet()) {
+			assertFacet(entry.getValue(), response, entry.getKey());
 		}
-		Assert.assertEquals(facetName, expected, toDateTime(time, expected.getZone()));
 	}
 
-	private DateTime toDateTime(long time, DateTimeZone timezone) {
-		return new DateTime(time, timezone);
+	private void index(String value) {
+		client.prepareIndex(index, type).setSource(field, value).execute().actionGet();
+		client.admin().indices().prepareRefresh().execute().actionGet();
+	}
+
+	private SearchResponse search(ImmutableMap<String, DateTime> intervals) {
+		SearchRequestBuilder request = client.prepareSearch(index).setQuery(QueryBuilders.matchAllQuery());
+		for (String interval : intervals.keySet()) {
+			request.addFacet(FacetBuilders.dateHistogramFacet(interval).field(field)
+				.interval(interval).preZone(timezone.toString()).preZoneAdjustLargeInterval(true));
+		}
+		return request.execute().actionGet();
+	}
+
+	private static void assertFacet(DateTime expected, SearchResponse response, String interval) {
+		DateHistogramFacet facet = response.facets().facet(interval);
+		long time = facet.getEntries().get(0).time();
+		Assert.assertEquals(interval, expected, new DateTime(time, expected.getZone()));
+	}
+
+	@After
+	public void close() {
+		client.close();
+		node.close();
 	}
 }
