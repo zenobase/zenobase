b34eafa4eb0dfa4bd4418c109a4ac1299b518e02
Eric Jain <ejain@zenobase.com>
2012-04-25 02:37:49 -0700

    Play 2.1-SNAPSHOT + LESS css

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "zeno.less")      
     )
 
 }
