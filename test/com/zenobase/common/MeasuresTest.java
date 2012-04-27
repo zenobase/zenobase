f994ef2083c9bc86f54e32ba7eaf898d30e13839
Eric Jain <ejain@zenobase.com>
2012-04-27 11:50:32 -0700

    fixed file encoding (UTF-8)

--- test/com/zenobase/common/MeasuresTest.java
+++ test/com/zenobase/common/MeasuresTest.java
@@ -30,10 +30,10 @@ public class MeasuresTest {
 		assertThatIsEqualTo("16093.44 m", "10 mi");
 		assertThatIsEqualTo("10000 m", "10 km");
 		assertThatIsEqualTo("68.0388555 kg", "150 lb");
-		assertThatIsEqualTo("294.2611111111112 K", "70 °F");
-		assertThatIsEqualTo("293.15 K", "20 °C");
-		assertThatIsEqualTo("0.001 m³", "1 L");
-		assertThatIsEqualTo("1 m³", "1 m³");
+		assertThatIsEqualTo("294.2611111111112 K", "70 Â°F");
+		assertThatIsEqualTo("293.15 K", "20 Â°C");
+		assertThatIsEqualTo("0.001 mÂ³", "1 L");
+		assertThatIsEqualTo("1 mÂ³", "1 mÂ³");
 	}
 
 	private void assertThatIsEqualTo(String expected, String value) {
