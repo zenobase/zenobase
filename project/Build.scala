01eb7289697cbeefdf46d5fd86c176ff0ba9685a
Eric Jain <ejain@zenobase.com>
2012-05-22 02:26:38 -0700

    added cloud-aws plugin

--- project/Build.scala
+++ project/Build.scala
@@ -15,12 +15,13 @@ object ApplicationBuild extends Build {
       "com.google.inject" % "guice" % "3.0",
       "com.google.inject.extensions" % "guice-multibindings" % "3.0",
       "org.elasticsearch" % "elasticsearch" % "0.19.3",
+      "org.elasticsearch" % "elasticsearch-cloud-aws" % "1.5.0",
       "org.mockito" % "mockito-all" % "1.9.0" % "test",
       "org.jvnet.mock-javamail" % "mock-javamail" % "1.9" % "test"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "zeno.less")      
+      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "zeno.less")
     )
 
 }
