2c7983eac42f2e9e6017e7ceb410e62f229f37d6
Eric Jain <ejain@zenobase.com>
2012-07-02 13:04:35 -0700

    record request/response

--- app/com/zenobase/actions/Timed.java
+++ app/com/zenobase/actions/Timed.java
@@ -1,12 +1,17 @@
 package com.zenobase.actions;
 
+import java.util.Enumeration;
+
 import play.Logger;
 import play.Logger.ALogger;
 import play.mvc.Action;
+import play.mvc.Http;
 import play.mvc.Http.Context;
 import play.mvc.Result;
 import com.google.common.base.Stopwatch;
 import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Request;
+import com.newrelic.api.agent.Response;
 import com.newrelic.api.agent.Trace;
 
 public class Timed extends Action.Simple {
@@ -16,17 +21,73 @@ public class Timed extends Action.Simple {
 	@Override
 	@Trace(dispatcher=true)
 	public Result call(Context context) throws Throwable {
-		NewRelic.setTransactionName("play", context.request().path());
+		NewRelic.setTransactionName("http", context.request().path());
 		try {
 			Stopwatch timer = new Stopwatch().start();
 			Result result = delegate.call(context);
 			timer.stop();
 			NewRelic.recordResponseTimeMetric("action", timer.elapsedMillis());
+			PlayRequest r = new PlayRequest(context);
+			NewRelic.setRequestAndResponse(r, r);
 			log.info(String.format("%s for %s", timer, context.request().uri()));
 			return result;
 		} catch (Throwable t) {
 			NewRelic.noticeError(t);
 			throw t;
+		}
+	}
+
+	public static class PlayRequest implements Request, Response {
+
+		private final Http.Context context;
+
+		public PlayRequest(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		public int getStatus() throws Exception {
+			return 0;
+		}
+
+		@Override
+		public String getStatusMessage() throws Exception {
+			return null;
+		}
+
+		@Override
+		public void setHeader(String name, String value) {
+
+		}
+
+		@Override
+		public String getRequestURI() {
+			return context.request().uri();
+		}
+
+		@Override
+		public String getHeader(String name) {
+			return context.request().getHeader(name);
+		}
+
+		@Override
+		public String getRemoteUser() {
+			return context.request().remoteAddress();
+		}
+
+		@Override
+		public Enumeration getParameterNames() {
+			return null;
+		}
+
+		@Override
+		public String[] getParameterValues(String name) {
+			return null;
+		}
+
+		@Override
+		public Object getAttribute(String name) {
+			return null;
 		}
 	}
 }
