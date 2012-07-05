c4f7e8f5f515825d5621cf1b833a1ef6c1d161f1
Eric Jain <ejain@zenobase.com>
2012-07-05 13:49:11 -0700

    upgraded elasticsearch to 0.19.8

--- project/Build.scala
+++ project/Build.scala
@@ -14,14 +14,15 @@ object ApplicationBuild extends Build {
       "com.google.guava" % "guava-testlib" % "12.0" % "test",
       "com.google.inject" % "guice" % "3.0",
       "com.google.inject.extensions" % "guice-multibindings" % "3.0",
-      "org.elasticsearch" % "elasticsearch" % "0.19.4",
-      "org.elasticsearch" % "elasticsearch-cloud-aws" % "1.5.0",
+      "org.elasticsearch" % "elasticsearch" % "0.19.8",
+      "org.elasticsearch" % "elasticsearch-cloud-aws" % "1.8.0",
       "org.mockito" % "mockito-all" % "1.9.0" % "test",
       "org.jvnet.mock-javamail" % "mock-javamail" % "1.9" % "test"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "zeno.less")
+      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "zeno.less"),
+      resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
     )
 
 }
