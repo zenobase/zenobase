50c206470bfc69ef32a8f8f11bfeec6e99e830d3
Eric Jain <ejain@zenobase.com>
2012-10-18 18:59:53 -0700

    debug elb status check requests

--- app/com/zenobase/actions/Canonical.java
+++ app/com/zenobase/actions/Canonical.java
@@ -1,4 +1,6 @@
 package com.zenobase.actions;
+import java.util.Map;
+
 import play.Logger;
 import play.api.mvc.Handler;
 import play.mvc.Http.RequestHeader;
@@ -12,11 +14,19 @@ public class Canonical {
 	private String baseUri;
 
 	public Handler redirect(RequestHeader request) {
-		Logger.info("redirect " + request.host() + request.uri() + " for " + request.getHeader("User-Agent"));
+		Logger.info("redirect " + request.host() + request.uri());
 		return controllers.Default.redirect(baseUri + request.uri());
 	}
 
 	public boolean test(RequestHeader request) {
+		if ("ELBLatencyCheck-1.0".equals(request.getHeader("User-Agent"))) {
+			Logger.info("status check:");
+			for (Map.Entry<String, String[]> entry : request.headers().entrySet()) {
+				for (String value : entry.getValue()) {
+					Logger.info("[http] " + entry.getKey() + ": " + value);
+				}
+			}
+		}
 		String scheme = request.getHeader("X-Forwarded-Proto");
 		return scheme == null || baseUri.equals(scheme + "://" + request.host());
 	}
