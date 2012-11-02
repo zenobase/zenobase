389dc7241664a4ef1f05c74629d0d72393852264
Eric Jain <ejain@zenobase.com>
2012-11-02 01:20:09 -0700

    add missing class

---
+++ app/com/zenobase/json/EnergyField.java
@@ -0,0 +1,10 @@
+package com.zenobase.json;
+
+import javax.measure.quantity.Energy;
+
+public class EnergyField extends MeasurementField<Energy> {
+
+	public EnergyField(String name) {
+		super(name);
+	}
+}
