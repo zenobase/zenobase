e1be30053c57eda688b1d7c84e1e48e0e6f9c5cf
Eric Jain <ejain@zenobase.com>
2012-12-15 14:07:11 -0800

    remove debug logging statement

--- app/com/zenobase/controllers/EventListController.java
+++ app/com/zenobase/controllers/EventListController.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 import org.codehaus.jackson.node.ObjectNode;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import play.Logger;
 import play.mvc.BodyParser;
 import play.mvc.Result;
 import play.mvc.With;
@@ -99,7 +98,6 @@ public class EventListController extends ControllerSupport {
 		if (!event.contains(Event.TIMESTAMP)) {
 			event.addValue(Event.TIMESTAMP, new DateTime(DateTimeZone.UTC));
 		}
-		Logger.info("e:" + event);
 		return new CreateEventCommand(principal, bucketId, event);
 
 	}
