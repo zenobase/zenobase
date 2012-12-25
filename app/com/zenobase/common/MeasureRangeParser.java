9214207a23e0a30081344864aff1121ea766c5a2
Eric Jain <ejain@zenobase.com>
2012-12-25 17:39:17 -0400

    fix generics-related compilation issue

--- app/com/zenobase/common/MeasureRangeParser.java
+++ app/com/zenobase/common/MeasureRangeParser.java
@@ -2,11 +2,12 @@ package com.zenobase.common;
 
 import javax.measure.DecimalMeasure;
 import javax.measure.Measurable;
+import javax.measure.quantity.Quantity;
 
-public class MeasureRangeParser extends RangeParser<Measurable<?>> {
+public class MeasureRangeParser extends RangeParser<Measurable<Quantity>> {
 
 	@Override
-	protected DecimalMeasure<?> getValue(String s) {
+	protected DecimalMeasure<Quantity> getValue(String s) {
 		return Measures.valueOf(s);
 	}
 }
--- app/com/zenobase/search/MeasureRangeConstraint.java
+++ app/com/zenobase/search/MeasureRangeConstraint.java
@@ -2,6 +2,7 @@ package com.zenobase.search;
 
 import javax.measure.DecimalMeasure;
 import javax.measure.Measurable;
+import javax.measure.quantity.Quantity;
 
 import com.google.common.collect.Range;
 
@@ -9,12 +10,12 @@ import com.zenobase.common.MeasureRangeParser;
 import com.zenobase.common.Measures;
 import com.zenobase.json.MeasurementField;
 
-public class MeasureRangeConstraint extends RangeConstraintSupport<Measurable<?>> {
+public class MeasureRangeConstraint extends RangeConstraintSupport<Measurable<Quantity>> {
 
 	private final MeasureRangeParser parser = new MeasureRangeParser();
 
 	@Override
-	protected Range<Measurable<?>> parseRange(String value) {
+	protected Range<Measurable<Quantity>> parseRange(String value) {
 		return parser.parse(value);
 	}
 
@@ -24,7 +25,7 @@ public class MeasureRangeConstraint extends RangeConstraintSupport<Measurable<?>
 	}
 
 	@Override
-	protected Number getValue(Measurable<?> value) {
-		return Measures.toStandard(((DecimalMeasure<?>) value)).getValue();
+	protected Number getValue(Measurable<Quantity> value) {
+		return Measures.toStandard(((DecimalMeasure<Quantity>) value)).getValue();
 	}
 }
