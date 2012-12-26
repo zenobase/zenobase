2122d3e2bbed06b5c534b7e81b21ee6a704f6f6a
Eric Jain <ejain@zenobase.com>
2012-12-25 20:00:08 -0400

    add test case

---
+++ test/com/zenobase/common/DateTimeRangeParserTest.java
@@ -0,0 +1,24 @@
+package com.zenobase.common;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+import com.google.common.collect.Range;
+import com.google.common.collect.Ranges;
+
+public class DateTimeRangeParserTest {
+
+	private final DateTimeRangeParser parser = new DateTimeRangeParser();
+
+	@Test
+	public void testYears() {
+		DateTime lower = DateTime.parse("2000-01-01TZ");
+		DateTime upper = DateTime.parse("2010-02-03T04:05:06.007-08:00");
+		testRange("[2000TZ..2010-02-03T04:05:06.007-08:00)", Ranges.closedOpen(lower, upper));
+	}
+
+	private void testRange(String value, Range<DateTime> expected) {
+		assertThat(parser.parse(value)).isEqualTo(expected);
+	}
+}
