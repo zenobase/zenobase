cb05a07b4b1541a0dcae20cb88454130058684d6
Eric Jain <ejain@zenobase.com>
2013-01-25 17:22:55 -0800

    action for clickjack protection

---
+++ app/com/zenobase/actions/NoFrame.java
@@ -0,0 +1,19 @@
+package com.zenobase.actions;
+
+import play.mvc.Action;
+import play.mvc.Http.Context;
+import play.mvc.Result;
+
+/**
+ * Protection against clickjacking attacks.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/HTTP/X-Frame-Options">The X-Frame-Options response header</a>
+ */
+public class NoFrame extends Action.Simple {
+
+	@Override
+	public Result call(Context context) throws Throwable {
+		context.response().setHeader("X-Frame-Options", "SAMEORIGIN");
+		return delegate.call(context);
+	}
+}
