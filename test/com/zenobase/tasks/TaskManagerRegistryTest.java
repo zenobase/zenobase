18f6f6f80bb4a82c9af202e75b99caef797643b2
Eric Jain <ejain@zenobase.com>
2013-01-31 12:38:28 -0800

    add unit test

---
+++ test/com/zenobase/tasks/TaskManagerRegistryTest.java
@@ -0,0 +1,28 @@
+package com.zenobase.tasks;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import com.google.common.collect.Sets;
+
+public class TaskManagerRegistryTest {
+
+	@Test
+	public void test() {
+		TaskManager manager = mockTaskManager("foo");
+		Set<TaskManager> managers = Sets.newHashSet();
+		managers.add(manager);
+		managers.add(mockTaskManager("bar"));
+		TaskManagerRegistry registry = new TaskManagerRegistry(managers);
+		assertThat(registry.find("foo")).isSameAs(manager);
+	}
+
+	private static TaskManager mockTaskManager(String type) {
+		TaskManager manager = Mockito.mock(TaskManager.class);
+		Mockito.when(manager.getType()).thenReturn(type);
+		return manager;
+	}
+}
