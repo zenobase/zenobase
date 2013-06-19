d1271c06a68223630452764643f7b16566c53556
Eric Jain <ejain@zenobase.com>
2013-06-19 12:56:54 -0700

    new relic 2.19.1

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object ApplicationBuild extends Build {
       "org.jsoup" % "jsoup" % "1.7.2",
       "net.sf.opencsv" % "opencsv" % "2.3",
       "org.scribe" % "scribe" % "1.3.3",
-      "newrelic.java-agent" % "newrelic-api" % "2.19.0",
+      "newrelic.java-agent" % "newrelic-api" % "2.19.1",
       "org.seleniumhq.selenium" % "selenium-chrome-driver" % "2.32.0" % "test",
       "org.apache.httpcomponents" % "httpcore" % "4.2.3" % "test",
       "org.mockito" % "mockito-all" % "1.9.5" % "test",
