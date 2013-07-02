ae1ffabf92265c08d3fb121185457a4efcb28a33
Eric Jain <ejain@zenobase.com>
2013-07-02 13:40:34 -0700

    elasticsearch 0.90.2

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ApplicationBuild extends Build {
       "com.google.guava" % "guava-testlib" % "14.0.1" % "test",
       "com.google.inject" % "guice" % "3.0",
       "com.google.inject.extensions" % "guice-multibindings" % "3.0",
-      "org.elasticsearch" % "elasticsearch" % "0.90.1",
+      "org.elasticsearch" % "elasticsearch" % "0.90.2",
       "org.elasticsearch" % "elasticsearch-cloud-aws" % "1.12.0",
       "org.elasticsearch" % "elasticsearch-lang-javascript" % "1.4.0" exclude("log4j", "log4j"),
       "org.jscience" % "jscience" % "4.3.1",
