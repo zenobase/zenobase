a6d2a61f3953e994833dee255a1ae4f37741d0eb
Eric Jain <ejain@zenobase.com>
2013-10-15 15:53:30 -0700

    support queries with relative timestamps

--- app/com/zenobase/common/DateTimeRangeParser.java
+++ app/com/zenobase/common/DateTimeRangeParser.java
@@ -8,8 +8,20 @@ public class DateTimeRangeParser extends RangeParser<ReadableInstant> {
 
 	@Override
 	protected ReadableInstant getValue(String s) {
-		return Characters.isDigits(s)
-			? new DateTime(Long.parseLong(s), DateTimeZone.UTC)
-			: DateTimeFormat.parse(s);
+		DateTime value = null;
+		if (s.charAt(0) == '+') {
+			value = now().plus(PeriodFormat.parse(s.substring(1)).toPeriod());
+		} else if (s.charAt(0) == '-') {
+			value = now().minus(PeriodFormat.parse(s.substring(1)).toPeriod());
+		} else if (Characters.isDigits(s)) {
+			value = new DateTime(Long.parseLong(s), DateTimeZone.UTC);
+		} else {
+			value = DateTimeFormat.parse(s);
+		}
+		return value;
+	}
+
+	protected DateTime now() {
+		return DateTime.now(DateTimeZone.UTC);
 	}
 }
--- test/com/zenobase/common/DateTimeRangeParserTest.java
+++ test/com/zenobase/common/DateTimeRangeParserTest.java
@@ -3,18 +3,31 @@ package com.zenobase.common;
 import static org.fest.assertions.Assertions.assertThat;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 import com.google.common.collect.Range;
 
 public class DateTimeRangeParserTest {
 
-	private final DateTimeRangeParser parser = new DateTimeRangeParser();
+	private final DateTime now = DateTime.now(DateTimeZone.UTC);
+	private final DateTimeRangeParser parser = new DateTimeRangeParser() {
+		@Override
+		protected DateTime now() {
+			return now;
+		}
+	};
 
 	@Test
 	public void testYears() {
 		DateTime lower = DateTime.parse("2000-01-01TZ");
 		DateTime upper = DateTime.parse("2010-02-03T04:05:06.007-08:00");
 		testRange("[2000TZ..2010-02-03T04:05:06.007-08:00)", Range.closedOpen(lower, upper));
+	}
+
+	@Test
+	public void testRelative() {
+		testRange("[-1y..+1y]", Range.closed(now.minusYears(1), now.plusYears(1)));
+		testRange("[-1y 2M..+3y 4h]", Range.closed(now.minusYears(1).minusMonths(2), now.plusYears(3).plusHours(4)));
 	}
 
 	private void testRange(String value, Range<DateTime> expected) {
