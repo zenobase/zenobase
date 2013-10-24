cc46754fdae32c06874f6fa5218e0f66f2ef3e75
Eric Jain <ejain@zenobase.com>
2013-10-24 01:06:38 -0700

    add missing tests

---
+++ test/com/zenobase/search/LocalDateTimeConstraintBuilderTest.java
@@ -0,0 +1,54 @@
+package com.zenobase.search;
+
+import static com.zenobase.testing.NodeAssert.assertThat;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.zenobase.models.Event;
+
+public class LocalDateTimeConstraintBuilderTest extends ConstraintBuilderTestSupport {
+
+	@Before
+	public void addEvents() {
+		addEvent("2012-01-05T12:00:00Z");
+		addEvent("2012-02-05T12:15:00Z");
+		addEvent("2012-01-06T05:00:00-08:00");
+	}
+
+	private void addEvent(String timestamp) {
+		Event event = new Event();
+		event.setValue(Event.TIMESTAMP, DateTime.parse(timestamp));
+		addEvent(event);
+	}
+
+	@Test
+	public void testFindJanuary() {
+		addConstraint("%s.month_of_year:%s", Event.TIMESTAMP, "1");
+		ObjectNode result = execute();
+		assertThat(result).path(Search.TOTAL.getName()).isEqualTo(2);
+	}
+
+	@Test
+	public void testFindThe5th() {
+		addConstraint("%s.day_of_month:%s", Event.TIMESTAMP, "5");
+		ObjectNode result = execute();
+		assertThat(result).path(Search.TOTAL.getName()).isEqualTo(2);
+	}
+
+	@Test
+	public void testFindSunday() {
+		addConstraint("%s.day_of_week:%s", Event.TIMESTAMP, "7");
+		ObjectNode result = execute();
+		assertThat(result).path(Search.TOTAL.getName()).isEqualTo(1);
+	}
+
+	@Test
+	public void testFindNoon() {
+		addConstraint("%s.hour_of_day:%s", Event.TIMESTAMP, "12");
+		ObjectNode result = execute();
+		assertThat(result).path(Search.TOTAL.getName()).isEqualTo(2);
+	}
+}
