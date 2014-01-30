989468466fba7ba5ef7d64cb3dc821900957e1ef
Eric Jain <ejain@zenobase.com>
2014-01-29 23:15:34 -0800

    format test data

--- test/com/zenobase/tasks/foursquare/FoursquareTest.java
+++ test/com/zenobase/tasks/foursquare/FoursquareTest.java
@@ -14,7 +14,7 @@ public class FoursquareTest extends TaskTestSupport {
 	public void test() {
 		OAuthTaskManager manager = new FoursquareTaskManager(newCredentialsManager());
 		ObjectNode settings = Nodes.newObject();
-		settings.put("marker", "2013-11-01");
+		settings.put("marker", "2014-01-27");
 		Task task = manager.newTask(bucketId, principal, settings);
 		print(manager.execute(task, getCredentials()).toJson());
 	}
