a00a2d5b66ddb72942e6f8c8c8793c944d01117b
Eric Jain <ejain@zenobase.com>
2014-02-12 13:03:43 -0800

    add social media links to verification mail

--- app/com/zenobase/mail/VerificationMailer.java
+++ app/com/zenobase/mail/VerificationMailer.java
@@ -31,7 +31,10 @@ public class VerificationMailer {
 			"  " + username + "\n\n" +
 			"Please verify your email address by opening the following link:\n\n" +
 			"  " + hostname + "/#/users/" + username + "/verify?key=" + key.getKey() + "\n\n" +
-			"Thanks!\n";
+			"Thanks!\n\n" +
+			"P.S. For regular updates, follow us on Twitter <https://twitter.com/zenobase>, " +
+			"Google+ <https://plus.google.com/+Zenobase> or Facebook <https://www.facebook.com/zenobase>.";
+
 		mailer.send(new Message(email, "Your Zenobase Account", text));
 	}
 }
