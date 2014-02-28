a985cc871fcea755a2f1d71adc1fcd8116cb5573
Eric Jain <ejain@zenobase.com>
2014-02-27 21:47:28 -0800

    process up to 1K events

--- app/com/zenobase/tasks/forecast/ForecastTaskManager.java
+++ app/com/zenobase/tasks/forecast/ForecastTaskManager.java
@@ -55,7 +55,7 @@ public class ForecastTaskManager extends TaskManager {
 		List<Command> updates = Lists.newArrayList();
 		ObjectField objects = new ObjectField("events");
 		String field = "timestamp";
-		Facet list = new ListFacet(objects.getName(), 0, 50, field, SortOrder.ASC);
+		Facet list = new ListFacet(objects.getName(), 0, 1000, field, SortOrder.ASC);
 		DateTime from = task.getFrom();
 		QueryBuilder query = new OffsetDateTimeRangeConstraintBuilder(field).build(Range.<ReadableInstant>greaterThan(from));
 		ObjectNode result = events.find(task.getBucketId(), new EventSearchBuilder().addConstraint(query, false).addFacet(list).buildSearch());
