4c9af586ef31ab90deedfd97356af843dd3b7d99
Eric Jain <ejain@zenobase.com>
2014-05-06 14:49:53 -0700

    delete obsolete rescuetime api keys from task settings

--- app/com/zenobase/commands/CreateTaskCommand.java
+++ app/com/zenobase/commands/CreateTaskCommand.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import play.Logger;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.Maps;
 
@@ -53,6 +55,14 @@ public class CreateTaskCommand extends Command {
 
 		@Override
 		public Command parse(ObjectNode node, int version) {
+
+			JsonNode settings = node.path("settings");
+			if (settings.isObject()) {
+				if (((ObjectNode) settings).remove("key") != null) {
+					Logger.warn("Deleted obsolete API key");
+				}
+			}
+
 			switch (version) {
 				case 2: return new CreateTaskCommand(node);
 			}
