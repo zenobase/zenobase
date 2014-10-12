617267a707ab247521ad169cd43be277c8ce33d9
Eric Jain <ejain@zenobase.com>
2014-10-11 18:52:19 -0700

    fix unit test

--- test/com/zenobase/tasks/netatmo/DevicesResultTest.java
+++ test/com/zenobase/tasks/netatmo/DevicesResultTest.java
@@ -22,13 +22,15 @@ public class DevicesResultTest extends ResultTestSupport {
 
 		Device dev0 = Iterables.get(devices, 0);
 		assertThat(dev0.getId()).as("id").isEqualTo("70:ee:50:00:80:ee");
+		assertThat(dev0.getModuleId()).as("module id").isNull();
 		assertThat(dev0.getLabel()).as("label").isEqualTo("Bedroom");
 		assertThat(dev0.getCreated()).as("created").isEqualTo(new DateTime("2013-03-28T23:40:07.000Z", DateTimeZone.UTC));
 		assertThat(dev0.getUpdated()).as("updated").isEqualTo(new DateTime("2013-04-03T04:27:16.000Z", DateTimeZone.forID("America/Los_Angeles")));
 		assertThat(dev0.getLocation()).as("location").isEqualTo(new Location("47.6097", "-122.3331"));
 
 		Device dev1 = Iterables.get(devices, 1);
-		assertThat(dev1.getId()).as("id").isEqualTo("02:00:00:00:7b:a2");
+		assertThat(dev1.getId()).as("id").isEqualTo("70:ee:50:00:80:ee");
+		assertThat(dev1.getModuleId()).as("module id").isEqualTo("02:00:00:00:7b:a2");
 		assertThat(dev1.getLabel()).as("label").isEqualTo("Bathroom");
 		assertThat(dev1.getCreated()).as("created").isEqualTo(new DateTime("2013-03-28T23:40:07.000Z", DateTimeZone.UTC));
 		assertThat(dev1.getUpdated()).as("updated").isEqualTo(new DateTime("2013-04-03T04:27:16.000Z", DateTimeZone.forID("America/Los_Angeles")));
