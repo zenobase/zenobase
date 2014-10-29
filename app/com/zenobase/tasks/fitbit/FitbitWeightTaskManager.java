e190dcb7b61e38812849170515c360e4bbad3935
Eric Jain <ejain@zenobase.com>
2014-10-29 11:47:24 -0700

    use today's date to sync fitbit-weight, if no device exists

--- app/com/zenobase/tasks/fitbit/FitbitWeightTaskManager.java
+++ app/com/zenobase/tasks/fitbit/FitbitWeightTaskManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
@@ -37,12 +38,12 @@ public class FitbitWeightTaskManager extends FitbitTaskManagerSupport<FitbitWeig
 	@Override
 	protected Command safeExecute(FitbitWeightTask task, OAuthCredentials credentials) {
 		List<Event> events = Lists.newArrayList();
+		FitbitProfileResult profile = getProfile(task, credentials);
 		LocalDate syncDate = getLastDate(DeviceType.SCALE, task, credentials);
 		if (syncDate == null) {
-			return null;
+			syncDate = new DateTime(profile.getTimezone()).toLocalDate();
 		}
 		LocalDate fromDate = getFromDate(task);
-		FitbitProfileResult profile = getProfile(task, credentials);
 		for (LocalDate date = fromDate; date.isBefore(syncDate); date = date.plusDays(1)) {
 			OAuthRequest request = new OAuthRequest(Verb.GET, "https://api.fitbit.com/1/user/-/body/date/" + date + ".json");
 			request.addHeader("Accept-Language", profile.getWeightLocale());
