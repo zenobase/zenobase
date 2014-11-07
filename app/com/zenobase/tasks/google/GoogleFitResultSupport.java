b54c35b3b1b7547b3e2e092b19992ec7348cc375
Eric Jain <ejain@zenobase.com>
2014-11-07 00:30:01 -0800

    register mapmyrun package and log new packages

--- app/com/zenobase/tasks/google/GoogleFitResultSupport.java
+++ app/com/zenobase/tasks/google/GoogleFitResultSupport.java
@@ -3,6 +3,7 @@ package com.zenobase.tasks.google;
 import org.elasticsearch.common.base.Objects;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import play.Logger;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
@@ -13,9 +14,10 @@ abstract class GoogleFitResultSupport {
 
 	private static final Resource DEFAULT_RESOURCE = new Resource("Google Fit", "https://fit.google.com/");
 	private static final ImmutableMap<String, Resource> RESOURCES = ImmutableMap.<String, Resource>builder()
-		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
-		.put("com.runtastic.android", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.fitnesskeeper.runkeeper.pro", new Resource("RunKeeper", "http://runkeeper.com/"))
+		.put("com.mapmyrun.android2", new Resource("MapMyRun", "http://www.mapmyrun.com/"))
+		.put("com.runtastic.android", new Resource("Runtastic", "https://www.runtastic.com/"))
+		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
 		.build();
 
 	protected final JsonNode node;
@@ -45,6 +47,9 @@ abstract class GoogleFitResultSupport {
 			resource = new Resource(title, detailsUrl);
 		} else if (packageName != null) {
 			resource = RESOURCES.get(packageName);
+			if (resource == null && !packageName.startsWith("com.google")) {
+				Logger.warn("Found new package: {}", packageName);
+			}
 		}
 		return Objects.firstNonNull(resource, DEFAULT_RESOURCE);
 	}
--- test/com/zenobase/tasks/google/GoogleFitTest.java
+++ test/com/zenobase/tasks/google/GoogleFitTest.java
@@ -9,13 +9,13 @@ import com.zenobase.tasks.TaskTestSupport;
 public class GoogleFitTest extends TaskTestSupport {
 
 	@Test
-	//@Ignore
+	@Ignore
 	public void testActivities() {
 		run(new GoogleFitActivitiesTaskManager(newCredentialsManager()), Nodes.newObject()
-			.put("marker", "2014-11-06")
+			.put("marker", "2014-11-05")
 			.put("timezone", "America/Los_Angeles")
 			.put("metric", true)
-			.put("derived", true));
+			.put("derived", false));
 	}
 
 	@Test
