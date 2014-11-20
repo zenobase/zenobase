5a750dff0248911958ca33bb1ffa09c09f013c84
Eric Jain <ejain@zenobase.com>
2014-11-20 10:49:59 -0800

    register nike+ source

--- app/com/zenobase/tasks/google/GoogleFitResultSupport.java
+++ app/com/zenobase/tasks/google/GoogleFitResultSupport.java
@@ -20,6 +20,7 @@ abstract class GoogleFitResultSupport {
 		.put("com.runtastic.android.pro2", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
 		.put("fi.polar.beat", new Resource("Polar Beat", "http://www.polar.com/beat/"))
+		.put("com.nike.plusgps", new Resource("Nike+", "http://nikeplus.nike.com/"))
 		.build();
 
 	protected final JsonNode node;
