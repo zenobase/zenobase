7a9f7bc8147747371494a145d6649291bc18daf3
Eric Jain <ejain@zenobase.com>
2014-12-13 16:45:05 -0800

    register "my tracks" source for google fit

--- app/com/zenobase/tasks/google/GoogleFitResultSupport.java
+++ app/com/zenobase/tasks/google/GoogleFitResultSupport.java
@@ -12,15 +12,16 @@ import com.zenobase.models.Resource;
 
 abstract class GoogleFitResultSupport {
 
-	private static final Resource DEFAULT_RESOURCE = new Resource("Google Fit", "https://fit.google.com/");
-	private static final ImmutableMap<String, Resource> RESOURCES = ImmutableMap.<String, Resource>builder()
+	private static final Resource DEFAULT_SOURCE = new Resource("Google Fit", "https://fit.google.com/");
+	private static final ImmutableMap<String, Resource> SOURCES = ImmutableMap.<String, Resource>builder()
 		.put("com.fitnesskeeper.runkeeper.pro", new Resource("RunKeeper", "http://runkeeper.com/"))
+		.put("com.google.android.maps.mytracks", new Resource("MyTracks", "https://google.com/"))
 		.put("com.mapmyrun.android2", new Resource("MapMyRun", "http://www.mapmyrun.com/"))
+		.put("com.nike.plusgps", new Resource("Nike+", "http://nikeplus.nike.com/"))
 		.put("com.runtastic.android", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.runtastic.android.pro2", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
 		.put("fi.polar.beat", new Resource("Polar Beat", "http://www.polar.com/beat/"))
-		.put("com.nike.plusgps", new Resource("Nike+", "http://nikeplus.nike.com/"))
 		.build();
 
 	protected final JsonNode node;
@@ -49,11 +50,11 @@ abstract class GoogleFitResultSupport {
 		if (title != null && detailsUrl != null) {
 			resource = new Resource(title, detailsUrl);
 		} else if (packageName != null) {
-			resource = RESOURCES.get(packageName);
+			resource = SOURCES.get(packageName);
 			if (resource == null && !packageName.startsWith("com.google")) {
 				Logger.warn("Found new package: {}", packageName);
 			}
 		}
-		return Objects.firstNonNull(resource, DEFAULT_RESOURCE);
+		return Objects.firstNonNull(resource, DEFAULT_SOURCE);
 	}
 }
