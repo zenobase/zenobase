55dfc1c5f8ce50ec97fac186477b03aa083d6690
Eric Jain <ejain@zenobase.com>
2015-02-16 23:12:22 -0800

    register package com.ryansteckler.perfectcinch

--- app/com/zenobase/tasks/google/GoogleFitResultSupport.java
+++ app/com/zenobase/tasks/google/GoogleFitResultSupport.java
@@ -26,6 +26,7 @@ abstract class GoogleFitResultSupport {
 		.put("com.runtastic.android", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.runtastic.android.pro2", new Resource("Runtastic", "https://www.runtastic.com/"))
 		.put("com.ryansteckler.heartsync", new Resource("HeartSync", "https://play.google.com/store/apps/details?id=com.ryansteckler.heartsync"))
+		.put("com.ryansteckler.perfectcinch", new Resource("Cinch Weight Loss and Fitness", "http://www.perfectcinch.com/"))
 		.put("com.sillens.shapeupclub", new Resource("Lifesum", "https://lifesum.com/"))
 		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
 		.put("com.vitrox.facion.gui", new Resource("What's My Heart Rate", "http://facion.net/wmhr"))
