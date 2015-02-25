32931043b4324bdc2a0e2e99d5b7bbe6b8998b63
Eric Jain <ejain@zenobase.com>
2015-02-24 23:06:56 -0800

    increase result scrolling size

--- app/com/zenobase/search/Search.java
+++ app/com/zenobase/search/Search.java
@@ -38,7 +38,7 @@ public class Search {
 	}
 
 	public void execute(Index index, final Callback<ObjectNode> callback) {
-		index.find(buildQuery(), callback, 500);
+		index.find(buildQuery(), callback, 1000);
 	}
 
 	private SearchSourceBuilder buildSearch() {
