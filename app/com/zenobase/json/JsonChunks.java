0b140786b70b0093a46bfb9c390573813553c9e4
Eric Jain <ejain@zenobase.com>
2015-03-04 11:37:55 -0800

    buffer chunks

--- app/com/zenobase/json/JsonChunks.java
+++ app/com/zenobase/json/JsonChunks.java
@@ -1,5 +1,6 @@
 package com.zenobase.json;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 
 import play.Logger;
@@ -23,19 +24,14 @@ public abstract class JsonChunks extends Results.ByteChunks {
 					Logger.info("Disconnected stream {}", id);
 				}
 			});
-			JsonStream stream = new JsonStream(new ChunksOutputStream(out));
-			Logger.info("Created json stream {}...", id);
+			JsonStream stream = new JsonStream(new BufferedOutputStream(new ChunksOutputStream(out), 1024 * 1024));
 			onReady(stream);
-			Logger.info("Closing json stream {}...", id);
 			stream.close();
-			Logger.info("Closed json stream {}...", id);
 		} catch (IOException e) {
-			Logger.info("Interrupted stream {}", id);
 			throw new RuntimeException(e);
 		} finally {
-			Logger.info("Closing stream {}...", id);
 			out.close();
-			Logger.info("Closed stream {}...", id);
+			Logger.info("Closed stream {}", id);
 		}
 	}
 
