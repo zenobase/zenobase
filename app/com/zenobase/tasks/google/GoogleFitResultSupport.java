2d2f2ffdd4ac1971e4e2dad0d1155370c9e475af
Eric Jain <ejain@zenobase.com>
2015-03-05 12:37:06 -0800

    register withings source

--- app/com/zenobase/tasks/google/GoogleFitResultSupport.java
+++ app/com/zenobase/tasks/google/GoogleFitResultSupport.java
@@ -32,6 +32,7 @@ abstract class GoogleFitResultSupport {
 		.put("com.sillens.shapeupclub", new Resource("Lifesum", "https://lifesum.com/"))
 		.put("com.strava", new Resource("Strava", "http://www.strava.com/"))
 		.put("com.vitrox.facion.gui", new Resource("What's My Heart Rate", "http://facion.net/wmhr"))
+		.put("com.withings.wiscale2", new Resource("Withings", "http://www.withings.com/us/"))
 		.put("fi.polar.beat", new Resource("Polar Beat", "http://www.polar.com/beat/"))
 		.put("si.modula.android.instantheartrate", new Resource("Azumio", "http://www.azumio.com/"))
 		.build();
