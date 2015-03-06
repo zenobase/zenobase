be0fa7d92d781fc86696b763b3c41e5f9a22a37c
Eric Jain <ejain@zenobase.com>
2015-03-05 22:35:50 -0800

    drop chunk logging

--- app/com/zenobase/json/JsonChunks.java
+++ app/com/zenobase/json/JsonChunks.java
@@ -2,27 +2,15 @@ package com.zenobase.json;
 
 import java.io.IOException;
 
-import play.Logger;
-import play.libs.F;
 import play.mvc.Results;
 
-import com.zenobase.common.Generator;
 import com.zenobase.io.ChunksOutputStream;
 
 public abstract class JsonChunks extends Results.ByteChunks {
 
-	private final String id = Generator.id();
-
 	@Override
 	public void onReady(Results.Chunks.Out<byte[]> out) {
 		try {
-			Logger.info("Connected stream {}", id);
-			out.onDisconnected(new F.Callback0() {
-				@Override
-				public void invoke() throws Throwable {
-					Logger.info("Disconnected stream {}", id);
-				}
-			});
 			JsonStream stream = new JsonStream(new ChunksOutputStream(out));
 			onReady(stream);
 			stream.close();
@@ -30,7 +18,6 @@ public abstract class JsonChunks extends Results.ByteChunks {
 			throw new RuntimeException(e);
 		} finally {
 			out.close();
-			Logger.info("Closed stream {}", id);
 		}
 	}
 
