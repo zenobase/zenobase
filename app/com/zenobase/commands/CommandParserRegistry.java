cda1a203d794b8434a0339a175bb2447afc16b60
Eric Jain <ejain@zenobase.com>
2016-02-13 12:31:36 -0800

    rename a user

--- app/com/zenobase/commands/CommandParserRegistry.java
+++ app/com/zenobase/commands/CommandParserRegistry.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
+import play.Logger;
 
 public class CommandParserRegistry {
 
@@ -31,8 +32,19 @@ public class CommandParserRegistry {
 		Preconditions.checkNotNull(type, "Missing type field in %s", node);
 		CommandParser parser = parsers.get(type.getName());
 		Preconditions.checkNotNull(parser, "Missing parser for type '%s'", type.getName());
+		renameUser("thomaghenry", "thomasghenry", node);
 		Command command = parser.parse(node, type.getVersion());
 		Preconditions.checkNotNull(command, "Missing parser for version %s of type '%s'", type.getVersion(), type.getName());
 		return command;
+	}
+
+	private static void renameUser(String from, String to, ObjectNode node) {
+		if (from.equals(node.path("parameters").path("username").textValue())) {
+			((ObjectNode) node.path("parameters")).put("username", to);
+			Logger.warn("updated the username", node);
+		} else if (from.equals(node.path("parameters").path("user").path("name").textValue())) {
+			((ObjectNode) node.path("parameters").path("user")).put("name", to);
+			Logger.warn("updated the user.name", node);
+		}
 	}
 }
