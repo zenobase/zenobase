23391750ca2ccd8763b80dc1b3c1aa0d41e7eef1
Eric Jain <ejain@zenobase.com>
2016-02-14 12:18:45 -0800

    delete old google credentials + force use of tls1.1 in scripts

--- app/com/zenobase/scripts/ClientSupport.java
+++ app/com/zenobase/scripts/ClientSupport.java
@@ -1,13 +1,24 @@
 package com.zenobase.scripts;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.net.ssl.SSLContext;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.zenobase.json.Nodes;
 
 public abstract class ClientSupport {
 
-	protected final HttpClient client = HttpClientBuilder.create().build();
+	protected final HttpClient client = buildClient();
 	protected final String host;
 	protected final String callback;
 	protected final String apiKey;
@@ -18,6 +29,22 @@ public abstract class ClientSupport {
 		callback = System.getProperty("api.callback", host + "/test/");
 		apiKey = System.getProperty("api.key");
 		token = System.getProperty("api.token");
+	}
+
+	private HttpClient buildClient() {
+		try {
+			SSLContext context = SSLContexts.custom().useTLS().build();
+			SSLConnectionSocketFactory f = new SSLConnectionSocketFactory(context,
+				new String[] { "TLSv1.1" }, null,
+				SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+			return HttpClients.custom().setSSLSocketFactory(f).build();
+		} catch (KeyManagementException | NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected static ObjectNode readObject(HttpResponse response) throws IOException {
+		return Nodes.readObject(EntityUtils.toByteArray(response.getEntity()));
 	}
 
 	public void run() throws IOException {
---
+++ app/com/zenobase/scripts/DeleteCredentials.java
@@ -0,0 +1,61 @@
+package com.zenobase.scripts;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Lists;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.joda.time.DateTime;
+
+import com.zenobase.tasks.Credentials;
+
+public class DeleteCredentials extends ClientSupport {
+
+	private final String type;
+	private final DateTime olderThan;
+
+	public DeleteCredentials(String type, DateTime olderThan) {
+		this.type = type;
+		this.olderThan = olderThan;
+	}
+
+	public static void main(String[] args) throws IOException {
+		new DeleteCredentials("google", DateTime.parse("2014-11-01T00:00:00Z")).run();
+	}
+
+	@Override
+	protected void doRun() throws IOException {
+		for (Credentials credentials : find("type:" + type)) {
+			if (credentials.getCreated().isBefore(olderThan)) {
+				delete(credentials.getId());
+			}
+		}
+	}
+
+	private List<Credentials> find(String query) throws IOException {
+		List<Credentials> credentials = Lists.newArrayList();
+		for (int offset = 0, limit = 100; credentials.addAll(find(query, offset, limit)); offset += limit) {}
+		System.out.format("Found %d credentials for <%s>\n", credentials.size(), query);
+		return credentials;
+	}
+
+	private List<Credentials> find(String query, int offset, int limit) throws IOException {
+		List<Credentials> credentials = Lists.newArrayList();
+		HttpGet request = new HttpGet(String.format("%s/credentials/?code=%s&q=%s&offset=%d&limit=%d", host, token, query, offset, limit));
+		HttpResponse response = client.execute(request);
+		for (JsonNode eventNode : readObject(response).path("items")) {
+			credentials.add(new Credentials((ObjectNode) eventNode));
+		}
+		return credentials;
+	}
+
+	private void delete(String credentialsId) throws IOException {
+		HttpDelete request = new HttpDelete(String.format("%s/credentials/%s?code=%s", host, credentialsId, token));
+		HttpResponse response = client.execute(request);
+		System.out.format("Deleted <%s>: %d\n", credentialsId, response.getStatusLine().getStatusCode());
+	}
+}
