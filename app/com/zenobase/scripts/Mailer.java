87a4c76734b82d20fae9afedea1cfc78dd425633
Eric Jain <ejain@zenobase.com>
2026-03-13 20:59:00 -0700

    upgrade aws sdk and testcontainers

--- app/com/zenobase/scripts/Mailer.java
+++ app/com/zenobase/scripts/Mailer.java
@@ -3,21 +3,20 @@ package com.zenobase.scripts;
 import java.io.File;
 import java.io.IOException;
 
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
-import com.amazonaws.services.simpleemail.model.Body;
-import com.amazonaws.services.simpleemail.model.Content;
-import com.amazonaws.services.simpleemail.model.Destination;
-import com.amazonaws.services.simpleemail.model.Message;
-import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.Body;
+import software.amazon.awssdk.services.ses.model.Content;
+import software.amazon.awssdk.services.ses.model.Destination;
+import software.amazon.awssdk.services.ses.model.Message;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 public class Mailer {
 
 	public static void main(String[] args) throws IOException {
 
-		AmazonSimpleEmailService client = new AmazonSimpleEmailServiceClient();
+		SesClient client = SesClient.create();
 
 		String from = "";
 		String body = Files.toString(new File("template.txt"), Charsets.UTF_8);
@@ -27,13 +26,15 @@ public class Mailer {
 			String username = fields[1];
 			String to = fields[2];
 			System.err.println("Mailing " + to + "...");
-			client.sendEmail(new SendEmailRequest()
-				.withSource(from)
-				.withDestination(new Destination().withToAddresses(String.format("%s <%s>", username, to)))
-				.withMessage(new Message()
-					.withSubject(new Content("Subject"))
-					.withBody(new Body(new Content(String.format(body, username))))
+			client.sendEmail(SendEmailRequest.builder()
+				.source(from)
+				.destination(Destination.builder().toAddresses(String.format("%s <%s>", username, to)).build())
+				.message(Message.builder()
+					.subject(Content.builder().data("Subject").build())
+					.body(Body.builder().text(Content.builder().data(String.format(body, username)).build()).build())
+					.build()
 				)
+				.build()
 			);
 		}
 	}
--- app/com/zenobase/services/OpenSearchClientFactory.java
+++ app/com/zenobase/services/OpenSearchClientFactory.java
@@ -1,28 +1,49 @@
 package com.zenobase.services;
 
+import java.net.URI;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.hc.core5.http.HttpHost;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
 import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.aws.AwsSdk2Transport;
+import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
 import org.opensearch.client.transport.httpclient5.ApacheHttpClient5Transport;
 import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
 import play.Logger;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
 
 public class OpenSearchClientFactory implements ClientFactory {
 
 	private final String host;
+	private final String region;
 
 	@Inject
-	public OpenSearchClientFactory(@Named("es.host") String host) {
+	public OpenSearchClientFactory(@Named("es.host") String host, @Named("es.snapshot.region") String region) {
 		this.host = host;
+		this.region = region;
 	}
 
 	@Override
 	public OpenSearchClient createClient() {
 		Logger.info("Connecting to {}...", host);
-		HttpHost httpHost = HttpHost.create(java.net.URI.create(host));
+		URI uri = URI.create(host);
+		if ("https".equals(uri.getScheme())) {
+			SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+			AwsSdk2Transport transport = new AwsSdk2Transport(
+				httpClient,
+				uri.getHost(),
+				"es",
+				Region.of(region),
+				AwsSdk2TransportOptions.builder().build()
+			);
+			return new OpenSearchClient(transport);
+		}
+		HttpHost httpHost = HttpHost.create(uri);
 		ApacheHttpClient5Transport transport = ApacheHttpClient5TransportBuilder
 			.builder(httpHost)
 			.setMapper(new JacksonJsonpMapper())
