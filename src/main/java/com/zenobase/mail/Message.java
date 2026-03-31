0d0fb8e9890934db546cce747e9cbdbb7b9f2d59
Eric Jain <ejain@zenobase.com>
2026-03-31 10:45:45 -0700

    clean up a record class

--- src/main/java/com/zenobase/mail/Message.java
+++ src/main/java/com/zenobase/mail/Message.java
@@ -4,14 +4,9 @@ import com.google.common.base.Preconditions;
 
 public record Message(String to, String subject, String text) {
 
-	public Message(String to, String subject, String text) {
-		this.to = Preconditions.checkNotNull(to);
-		this.subject = Preconditions.checkNotNull(subject);
-		this.text = Preconditions.checkNotNull(text);
-	}
-
-	@Override
-	public boolean equals(Object that) {
-		return that instanceof Message m && to.equals(m.to()) && subject.equals(m.subject()) && text.equals(m.text());
+	public Message {
+		Preconditions.checkNotNull(to);
+		Preconditions.checkNotNull(subject);
+		Preconditions.checkNotNull(text);
 	}
 }
