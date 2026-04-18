58dafe4767769fcd942817bd34162dfd0cf4c860
Eric Jain <ejain@zenobase.com>
2026-04-18 15:34:59 -0700

    Enforce EnumOrdinal

--- src/main/java/com/zenobase/models/Role.java
+++ src/main/java/com/zenobase/models/Role.java
@@ -1,11 +1,17 @@
 package com.zenobase.models;
 
 public enum Role {
-	VIEWER,
-	CONTRIBUTOR,
-	OWNER;
+	VIEWER(1),
+	CONTRIBUTOR(2),
+	OWNER(3);
+
+	private final int level;
+
+	Role(int level) {
+		this.level = level;
+	}
 
 	public boolean implies(Role other) {
-		return other.ordinal() <= ordinal();
+		return other.level <= level;
 	}
 }
